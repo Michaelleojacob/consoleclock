ad79910986292d5a3e29012ff38a2f99bee4d738
Michael <michaelleojacob@gmail.com>
2021-07-13 16:11:30 -0700

    class version complete. iife complete

--- class.js
+++ class.js
@@ -1,9 +1,30 @@
-class Clock{
-    constructor(date, hours){
-        this.date = new Date();
-        this.hours = date.getHours();
+class Clock {
+    constructor({ template }) {
+        this.template = template;
     }
-    method1(){
-
+    render() {
+        let date = new Date();
+        let hours = date.getHours();
+        let mins = date.getMinutes();
+        let secs = date.getSeconds();
+        if (hours < 10) hours = '0' + hours;
+        if (mins < 10) mins = '0' + mins;
+        if (secs < 10) secs = '0' + secs;
+        let output = this.template
+        .replace('h', hours)
+        .replace('m', mins)
+        .replace('s', secs);
+        console.log(output);
+    }
+    stop() {
+        clearInterval(this.timer);
+    }
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
     }
 }
+
+
+let clock = new Clock({template: 'h:m:s'});
+clock.start();
--- function.js
+++ function.js
@@ -4,10 +4,10 @@ function Clock({ template }) {
     function render() {
         let date = new Date();
         let hours = date.getHours();
-        if (hours < 10) hours = '0' + hours;
         let mins = date.getMinutes();
-        if (mins < 10) mins = '0' + mins;
         let secs = date.getSeconds();
+        if (hours < 10) hours = '0' + hours;
+        if (mins < 10) mins = '0' + mins;
         if (secs < 10) secs = '0' + secs;
         let output = template
         .replace('h', hours)
